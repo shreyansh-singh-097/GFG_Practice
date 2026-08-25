fe6781c1d5a72d5a751b2e69e5b35e06507f25c1
Shreyansh Singh <shreyansh.singh.r06@gmail.com>
2026-08-25 16:23:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -4,13 +4,13 @@ public:
         int n = mat.size();
         for (int i = 0; i < n; i++) {
             bool row = true;
-            bool col = true; // Row check -> Celebrity kisi ko nahi janta
+            bool col = true; // Row check : Celebrity kisi ko nahi janta
             for (int j = 0; j < n; j++) {
                 if (i != j && mat[i][j] == 1) {
                     row = false;
                     break;
                 }
-            } // Column check -> Sab celebrity ko jante hain
+            } // Column check : Sab celebrity ko jante hain
             for (int j = 0; j < n; j++) {
                 if (i != j && mat[j][i] == 0) {
                     col = false;

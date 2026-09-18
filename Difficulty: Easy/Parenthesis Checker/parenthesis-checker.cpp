0901a1677442fb7013a89217f3e84fd85149373b
Shreyansh Singh <shreyansh.singh.r06@gmail.com>
2026-09-18 07:38:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+        stack<char> st;
+                for (int i = 0; i < s.size(); i++) {
+                    if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                        st.push(s[i]);
+                    } else {
+                        if (st.empty()) {
+                            return false;
+                        }
+                        if ((st.top() == '(' && s[i] == ')') ||(st.top() == '{' && s[i]== '}') ||(st.top() == '[' && s[i] == ']')) {
+                            st.pop();
+                        } else {return false;}
+                    }
+                }
+                return st.empty();
+    }
+};
